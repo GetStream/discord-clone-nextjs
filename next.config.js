2be28364b7fde2775a5d629d7f16c91370f314f7
Stefan <stefan.blos@gmail.com>
2023-11-29 17:56:58 +0100

    Allow images from Unsplash

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
