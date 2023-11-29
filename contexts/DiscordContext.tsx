cae9c417b012ba4c16e04320031109c299bc5b42
Stefan <stefan.blos@gmail.com>
2023-11-29 17:57:31 +0100

    Create and inject a Discord Context

---
+++ contexts/DiscordContext.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import { DiscordServer } from '@/app/page';
+import { createContext, useContext, useState } from 'react';
+import { StreamChat } from 'stream-chat';
+
+type DiscordState = {
+  server?: DiscordServer;
+  changeServer: (server: DiscordServer) => void;
+  createServer: (client: StreamChat, name: string, imageUrl: string) => void;
+};
+
+const initialValue: DiscordState = {
+  server: undefined,
+  changeServer: () => {},
+  createServer: () => {},
+};
+
+const DiscordContext = createContext<DiscordState>(initialValue);
+
+export const DiscordContextProvider: any = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [myState, setMyState] = useState<DiscordState>(initialValue);
+
+  function changeServer(server: DiscordServer) {
+    setMyState({ ...myState, server });
+  }
+
+  async function createServer(
+    client: StreamChat,
+    name: string,
+    imageUrl: string
+  ) {
+    if (client.userID) {
+      const channel = client.channel('messaging', 'randomId', {
+        name: 'Welcome',
+        members: [client.userID, 'test-user'],
+        data: {
+          image: imageUrl,
+          server: name,
+          category: 'Text Channels',
+        },
+      });
+      try {
+        const response = await channel.create();
+        console.log('[createServer] Response: ', response);
+        changeServer({ name, image: imageUrl });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+
+  const store: DiscordState = {
+    server: myState.server,
+    changeServer: changeServer,
+    createServer: createServer,
+  };
+
+  return (
+    <DiscordContext.Provider value={store}>{children}</DiscordContext.Provider>
+  );
+};
+
+export const useDiscordContext = () => useContext(DiscordContext);
