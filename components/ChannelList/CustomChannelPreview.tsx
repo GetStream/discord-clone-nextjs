7a24aa8e2ba6d62f8c265f05b3b9a63acfaf37e1
Stefan <stefan.blos@gmail.com>
2024-01-24 12:28:07 +0100

    Added unread indicator

--- components/ChannelList/CustomChannelPreview.tsx
+++ components/ChannelList/CustomChannelPreview.tsx
@@ -5,16 +5,25 @@ import {
 
 const CustomChannelPreview = (props: ChannelPreviewUIComponentProps) => {
   console.log('[CustomChannelPreview]');
+
   const { channel } = props;
   const { setActiveChannel } = useChatContext();
   return (
-    <button
-      className='w-full flex items-center px-2 hover:bg-gray-200 rounded-md'
-      onClick={() => setActiveChannel(channel)}
+    <div
+      className={`flex items-center mx-2 ${
+        props.channel.countUnread() > 0 ? 'channel-container' : ''
+      }`}
     >
-      <span className='italic text-xl mr-2 text-gray-500'>#</span>
-      <span className='text-sm'>{channel.data?.name || 'Channel Preview'}</span>
-    </button>
+      <button
+        className='w-full flex items-center px-2 hover:bg-gray-200 rounded-md'
+        onClick={() => setActiveChannel(channel)}
+      >
+        <span className='italic text-xl mr-2 text-gray-500'>#</span>
+        <span className='text-sm'>
+          {channel.data?.name || 'Channel Preview'}
+        </span>
+      </button>
+    </div>
   );
 };
 
