5dc3e0933055051690c9f465cc2f31cd4ac81c3f
Stefan <stefan.blos@gmail.com>
2024-03-28 22:39:08 +0100

    UI for adding call in list

--- components/ChannelList/CallList/CallList.tsx
+++ components/ChannelList/CallList/CallList.tsx
@@ -1,8 +1,7 @@
 import { useDiscordContext } from '@/contexts/DiscordContext';
 import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
 import { useCallback, useEffect, useState } from 'react';
-import { ChevronRight, Speaker } from '../Icons';
-import { TrackPublished } from '@stream-io/video-client/dist/src/gen/video/sfu/event/events';
+import { ChevronRight, PlusIcon, Speaker } from '../Icons';
 
 export default function CallList(): JSX.Element {
   const { server, createCall } = useDiscordContext();
@@ -30,21 +29,45 @@ export default function CallList(): JSX.Element {
 
   return (
     <div className='w-full my-2'>
-      <button
-        className='flex w-full items-center justify-start mb-1'
-        onClick={() => setIsOpen((currentValue) => !currentValue)}
-      >
-        <div
-          className={`${
-            isOpen ? 'rotate-90' : ''
-          } transition-all ease-in-out duration-200`}
+      <div className='flex'>
+        <button
+          className='flex w-full items-center justify-start mb-1 px-2'
+          onClick={() => setIsOpen((currentValue) => !currentValue)}
         >
-          <ChevronRight />
-        </div>
-        <h2 className='inline-block uppercase text-sm font-bold px-2'>
-          Voice Channels
-        </h2>
-      </button>
+          <div
+            className={`${
+              isOpen ? 'rotate-90' : ''
+            } transition-all ease-in-out duration-200`}
+          >
+            <ChevronRight />
+          </div>
+          <h2 className='inline-block uppercase text-sm font-bold px-2'>
+            Voice Channels
+          </h2>
+        </button>
+        <button
+          onClick={async () => {
+            if (!client) {
+              console.log('No client');
+              return;
+            }
+            await createCall(
+              client,
+              server?.name || 'Test Server',
+              'Test Call 4',
+              [
+                'user_2dwxDiRGTaQ79kCJyldJq1DEYv8',
+                '38391e52-68a0-4bc5-9d57-61d9da1adf3a',
+                '5af8f586-f1da-4bdf-a636-add588765d70',
+                '3dcd3e0c-d9d3-4886-ae44-ae4022a214bd',
+              ]
+            );
+            loadAudioChannels();
+          }}
+        >
+          <PlusIcon className='mr-2 h-5 w-5 text-gray-500' />
+        </button>
+      </div>
       {isOpen && (
         <div className='px-2'>
           {calls.map((call) => (
@@ -61,28 +84,6 @@ export default function CallList(): JSX.Element {
           ))}
         </div>
       )}
-      <button
-        onClick={async () => {
-          if (!client) {
-            console.log('No client');
-            return;
-          }
-          await createCall(
-            client,
-            server?.name || 'Test Server',
-            'Test Call 4',
-            [
-              'user_2dwxDiRGTaQ79kCJyldJq1DEYv8',
-              '38391e52-68a0-4bc5-9d57-61d9da1adf3a',
-              '5af8f586-f1da-4bdf-a636-add588765d70',
-              '3dcd3e0c-d9d3-4886-ae44-ae4022a214bd',
-            ]
-          );
-          loadAudioChannels();
-        }}
-      >
-        Create new call
-      </button>
     </div>
   );
 }
